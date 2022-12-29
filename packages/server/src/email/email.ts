30e33891b19fd3ca58cc760c5611bf9cb0d23b16
Sabine Laszakovits <sabinbox@laszakovits.net>
2022-12-29 14:51:18 +0100

    changed email sending to postmark

--- packages/server/src/email/email.ts
+++ packages/server/src/email/email.ts
@@ -1,4 +1,6 @@
 import { SendEmailCommand, SESv2Client } from '@aws-sdk/client-sesv2';
+import { createTransport as createNodemailerTransport } from 'nodemailer';
+import { default as createPostmarkTransport } from 'nodemailer-postmark-transport';
 import MailComposer from 'nodemailer/lib/mail-composer';
 import Mail, { Address } from 'nodemailer/lib/mailer';
 import { getConfig } from '../config';
@@ -13,21 +15,36 @@ import { logger } from '../logger';
  * @param options The MailComposer options.
  */
 export async function sendEmail(options: Mail.Options): Promise<void> {
-  const sesClient = new SESv2Client({ region: 'us-east-1' });
   const fromAddress = getConfig().supportEmail;
   const toAddresses = buildAddresses(options.to);
   const ccAddresses = buildAddresses(options.cc);
   const bccAddresses = buildAddresses(options.bcc);
   logger.info(`Sending email to ${toAddresses?.join(', ')} subject "${options.subject}"`);
 
-  const msg = await buildRawMessage(
-    await rewriteAttachments(RewriteMode.PRESIGNED_URL, systemRepo, {
+  const options_with_rewritten_attachments: Mail.Options = await rewriteAttachments(
+    RewriteMode.PRESIGNED_URL, 
+    systemRepo, 
+    {
       ...options,
       from: fromAddress,
       sender: fromAddress,
-    })
+    },
   );
 
+  if (process.env.POSTMARK_API_TOKEN) {
+    const client = createNodemailerTransport(
+      createPostmarkTransport({
+        auth: {
+          apiKey: process.env.POSTMARK_API_TOKEN, 
+        }, 
+        // postmarkOptions: {}, 
+      }),
+    );
+    await client.sendMail(options_with_rewritten_attachments);
+  } 
+  else {
+    const sesClient = new SESv2Client({ region: 'us-east-1' });
+    const msg = await buildRawMessage(options_with_rewritten_attachments);
   await sesClient.send(
     new SendEmailCommand({
       FromEmailAddress: fromAddress,
@@ -43,6 +60,7 @@ export async function sendEmail(options: Mail.Options): Promise<void> {
       },
     })
   );
+  }
 }
 
 function buildAddresses(input: string | Address | Array<string | Address> | undefined): string[] | undefined {
